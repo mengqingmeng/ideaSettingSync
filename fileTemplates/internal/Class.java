f42824ae36dc3af9d856f4bad5a86759c72b84c3
孟庆猛 <762577099@qq.com>
2020-04-11 10:10:54 +0800

    IU-2020.1 <mqm@MQMdeMacBook-Pro.local Create baseRefactoring.xml, ide.general.xml, _@user_Default.icls, project.default.xml, Enum.java, Interface.java, lombok-plugin.xml, Default.xml, find.xml, filetypes.xml, projectView.xml, colors.scheme.xml, path.macros.xml, databaseDrivers.xml, github.xml, mavenVersion.xml, AnnotationType.java, Default.xml, studioFlags.xml, Class.java, jdk.table.xml, laf.xml, Mac OS X 10_5_ copy.xml, proxy.settings.xml, vcs.xml, keymap.xml, mybatis.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @author ${USER}
+ * @description
+ * @date ${DATE} ${TIME}
+ * @copyright TNIT
+ * @since 1.0
+ */
+public class ${NAME} {
+}
